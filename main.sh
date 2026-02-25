99d225af6fdacd36e1f368038df7a0f6fbce51d3
Hafizi Ruslan <pizi@hotmail.my>
2026-02-25 23:36:10 +0800

    feat(main): trigger environment recreation when pyproject.toml changes and clear python bytecode cache on sync
    - After a successful update and when HEAD matches REMOTE, detect local changes to pyproject.toml and delete .venv to force environment recreation.
    - During dependency synchronization with internet access, remove __pycache__ directories to clear Python bytecode cache.

--- main.sh
+++ main.sh
@@ -176,6 +176,11 @@ if [ "$INTERNET_AVAILABLE" = true ] && check_disk_space; then
       fi
 
       if [ "$UPDATE_SUCCESS" = true ] && [ "$(sudo -u $dir_own git rev-parse HEAD)" = "$REMOTE" ]; then
+        if ! sudo -u $dir_own git diff --quiet "$LOCAL" HEAD -- pyproject.toml; then
+          log_msg INFO "Application updated. Forcing environement recreation."
+          rm -rf .venv
+        fi
+
         log_msg INFO "Verifying repository integrity..."
         if sudo -u $dir_own git fsck --full >/dev/null 2>&1; then
           log_msg INFO "Update applied and verified. Restarting script..."
@@ -238,6 +243,11 @@ fi
 sync_dependencies() {
   local action=$1
   if [ "$INTERNET_AVAILABLE" = true ]; then
+    log_msg INFO "Clearing Python bytecode cache"
+    find . -name '__pycache__' -type d -print0 | while IFS= read -r -d $'\0' dir; do
+      rm -rf "$dir"
+    done
+
     log_msg INFO "$action RasPiAPRS dependencies"
     sudo -u $dir_own uv sync -q
   elif [ "$action" = "Installing" ]; then
